34ec700bcdf84eeb72ccd30d6291f0071da04324
Enver <en.eredzhep@mail.ru>
2024-01-15 10:54:37 +0300

    Исправление ошибок

---
+++ habrTests/habrTest/src/test/java/org/example/habrtest/tests/MainPageTest.java
@@ -0,0 +1,187 @@
+package org.example.habrtest.tests;
+
+import org.example.habrtest.MyExtension;
+import org.example.habrtest.pages.MainPage.*;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MyExtension.class)
+public class MainPageTest extends BaseTest {
+    private MainPage mainPage;
+    private LogInPage logInPage;
+    private HabrMyFeedPage habrMyFeedPage;
+    private HabrSignUpPage habrSignUpPage;
+    private HabrPasswordRecoveryPage habrPasswordRecoveryPage;
+    private HabrAllFlowsPage habrAllFlowsPage;
+
+    @BeforeEach
+    @Override
+    public void setUp() {
+        super.setUp();
+        getDriver().get("https://habr.com/ru/feed/");
+        mainPage = new MainPage(getDriver());
+        logInPage = new LogInPage(getDriver());
+        habrMyFeedPage = new HabrMyFeedPage(getDriver());
+        habrPasswordRecoveryPage = new HabrPasswordRecoveryPage(getDriver());
+        habrSignUpPage = new HabrSignUpPage(getDriver());
+        habrAllFlowsPage = new HabrAllFlowsPage(getDriver());
+    }
+
+    @Test
+    @Tag("1")
+    @DisplayName("Тестирование кнопки Войти")
+    public void shapowBoxFind() {
+        mainPage.logInClick();
+        assertTrue(logInPage.shadowBoxIsVisible(), "Окно логина не отобразилось");
+    }
+
+    @Test
+    @Tag("2")
+    @DisplayName("Тестирование появления окна Восстановления пароля")
+    public void passwordRecoveryWindowIsDisplayed() {
+        mainPage.logInClick();
+        logInPage.clickForgotPasswordButton();
+        assertTrue(habrPasswordRecoveryPage.recoveryFieldIsDisplayed(), "Попап окно Востановление пароля не появилось");
+    }
+
+    @Test
+    @Tag("3")
+    @DisplayName("Возврат с поля восстановления пароля на поля логина")
+    public void comeBackToLogInPage() {
+        mainPage.logInClick();
+        logInPage.clickForgotPasswordButton();
+        habrPasswordRecoveryPage.comeBackToLoginPage();
+        assertTrue(logInPage.shadowBoxIsVisible(), "Окно логина не отобразилось");
+    }
+
+    @Test
+    @Tag("3")
+    @DisplayName("Переход в окно регистрации с попап окна Восстановление пароля")
+    public void goToSignUpFromRecoveryPassswordWindow() {
+        String actualTitle = "Регистрация";
+        mainPage.logInClick();
+        logInPage.clickForgotPasswordButton();
+        habrPasswordRecoveryPage.signUpButtonClick();
+        assertEquals(actualTitle, habrSignUpPage.getSignUpShadowBoxTitle(), "Заголовок на странице регистрации не совпадает");
+    }
+
+    @Test
+    @Tag("4")
+    @DisplayName("Восстановление пароля, кнопка Отправить не активна")
+    public void recoveryPageSendButtonIsDisabled() {
+        mainPage.logInClick();
+        logInPage.clickForgotPasswordButton();
+        assertFalse(habrPasswordRecoveryPage.subnitButtonIsDisable(), "Кнопка Отправить активна");
+
+    }
+
+    @Test
+    @Tag("5")
+    @DisplayName("Восстановление пароля, кнопка Отправить активна если в инпут ввести любой текст")
+    public void recoveryPageSendButtonIsEnable() {
+        String yourEmail = "usertestovich@mail.ru";
+        mainPage.logInClick();
+        logInPage.clickForgotPasswordButton();
+        habrPasswordRecoveryPage.setEmailInputField(yourEmail);
+        assertTrue(habrPasswordRecoveryPage.subnitButtonIsDisable(), "Кнопка Отправить не активна");
+
+    }
+
+    @Test
+    @Tag("6")
+    @DisplayName("Регистрация, кнопка Зарегестрировать  не активна если в инпутgens не заполнены")
+    public void signUpButtonIsDisabled() {
+        mainPage.logInClick();
+        logInPage.clickForgotPasswordButton();
+        habrPasswordRecoveryPage.signUpButtonClick();
+        assertFalse(habrSignUpPage.registrationButtonIsDisabled(), "Кнопка Зарегестрироваться активна");
+    }
+
+    @Test
+    @Tag("7")
+    @DisplayName("Регистрация, кнопка Зарегестрировать   активна если все инпуты заполнены")
+    public void signUpButtonIsEnable() {
+        String yourEmail = "usertestovich@mail.ru";
+        String yourNick = "monk";
+        String yourPassword = "12345678";
+        mainPage.logInClick();
+        logInPage.clickForgotPasswordButton();
+        habrPasswordRecoveryPage.signUpButtonClick();
+        habrSignUpPage.setEmailInputField(yourEmail);
+        habrSignUpPage.setNickInputField(yourNick);
+        habrSignUpPage.setPasswordInputField(yourPassword);
+        habrSignUpPage.setPasswordRepeatInputField(yourPassword);
+        assertTrue(habrSignUpPage.registrationButtonIsDisabled(), "Кнопка Зарегестрироваться не активна");
+    }
+
+    @Test
+    @Tag("8")
+    @DisplayName("Появление Попап окна с настройками страницы ")
+    public void totgglePopapWindowArize() {
+        mainPage.userMenuToggleButtonClick();
+        assertTrue(mainPage.popapToggleWindowIsDisplayed(), "Попап окно с настройками не появилось");
+    }
+
+    @Test
+    @Tag("9")
+    @DisplayName("Смена языка интерфейса")
+    public void switchInterfaceLanguage() {
+        String expectedlChangedToggleSummary = "Page settings";
+        mainPage.userMenuToggleButtonClick();
+        mainPage.clickRadioButtonEnglishInterface();
+        assertEquals(expectedlChangedToggleSummary, mainPage.getTogglePopapWidowSummary(), "Заголовок интерфейса не соответствует");
+    }
+
+    @Test
+    @Tag("10")
+    @DisplayName("Сохранение изменений языка интерфейса")
+    public void changeInterfaceLanguage() {
+        String expectedlMyFeedSummary = "My feed";
+        mainPage.userMenuToggleButtonClick();
+        mainPage.clickRadioButtonEnglishInterface();
+        mainPage.saveButtonClick();
+        assertEquals(expectedlMyFeedSummary, mainPage.getMyFeedSummary(), "Заголовок на странице не соответствует, язык не изменен");
+    }
+
+    @Test
+    @Tag("11")
+    @DisplayName("Возвращение текущих настроек - изменений языка интерфейса")
+    public void changeInterfaceLanguage1() {
+        String expectedlMyFeedSummary = "Моя лента";
+        mainPage.userMenuToggleButtonClick();
+        mainPage.clickRadioButtonRussianInterface();
+        mainPage.saveButtonClick();
+        assertEquals(expectedlMyFeedSummary, mainPage.getMyFeedSummary(), "Заголовок на странице не соответствует, язык не изменен");
+    }
+
+    @Test
+    @Tag("12")
+    @DisplayName("При помощи поиска, ввести  и отправить запрос")
+    public void findRequst() {
+        String yourRequest = "Reforge";
+        mainPage.searchButtonClick();
+        mainPage.findYourRequest(yourRequest);
+        assertEquals(yourRequest, mainPage.getSearchFieldText(), "Тест в поле поиска не соответствует");
+    }
+
+    @Test
+    @Tag("13")
+    @DisplayName("Раскрытие настроек ленты")
+    public void openFeedProperty() {
+        mainPage.clickFeedProperty();
+        assertTrue(mainPage.feedPropertyIsDisplayed(), "Окно с настройками не отобразились");
+    }
+
+    @Test
+    @Tag("14")
+    @DisplayName("Раскрытие настроек ленты")
+    public void flowsSuammaryDisplay() {
+        mainPage.clickAllFlows();
+        assertTrue(habrAllFlowsPage.flowsSummaryIsDisplayed(), "Заголовок на странице с потоками не отобразился");
+    }
+
+
+
+}
