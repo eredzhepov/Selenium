342554b7fd1b712d9ad67e193c53b6a1aa3490fb
Enver <en.eredzhep@mail.ru>
2024-01-15 10:52:22 +0300

    Добавлено 14 тестов

---
+++ habrTests/habrTest/src/test/java/org/example/habrtest/pages/MainPage/HabrAllFlowsPage.java
@@ -0,0 +1,28 @@
+package org.example.habrtest.pages.MainPage;
+
+import org.example.habrtest.AllureLogger;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+
+public class HabrAllFlowsPage {
+    WebDriver driver;
+    AllureLogger LOG = new AllureLogger(LoggerFactory.getLogger(HabrAllFlowsPage.class));
+    @FindBy(css = ".tm-section-name__text")
+    private WebElement allFlowsSummary;
+    public Boolean flowsSummaryIsDisplayed(){
+        LOG.info("Отображение заголовка на странице с потоками");
+        return allFlowsSummary.isDisplayed();
+    }
+    @FindBy(css = ".tm-tabs__tab-link.tm-tabs__tab-link")
+    private List<WebElement> menuElements;
+    public HabrAllFlowsPage(WebDriver driver){
+        this.driver = driver;
+        PageFactory.initElements(driver,this);
+    }
+
+}
